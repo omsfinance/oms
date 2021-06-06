dd8e4839235162baa43e8824b27551c54d9c5dfa
Crypto Shifu <cryptoshifu@gmail.com>
2021-06-06 19:40:05 +0800

    Updated to Mainnet USDC Address

--- contracts/v6/Oracle.sol
+++ contracts/v6/Oracle.sol
@@ -21,7 +21,7 @@ contract Oracle {
     IUniswapV2Pair immutable pair;
     address public immutable token0;
     address public immutable token1;
-    address internal usdc = 0xe22da380ee6B445bb8273C81944ADEB6E8450422;
+    address internal usdc = 0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48;
 
     uint    public price0CumulativeLast;
     uint    public price1CumulativeLast;
