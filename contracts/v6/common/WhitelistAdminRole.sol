87db20804b795eb934de518ac0386919137c02b2
Jagveer Singh <jagveer@jagveer.singh>
2021-08-01 00:48:54 +0530

    added keepers oracle policy contract

---
+++ contracts/v6/common/WhitelistAdminRole.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: Unlicensed
+pragma solidity ^0.6.12;
+
+import "../library/Roles.sol";
+import "./Context.sol";
+
+/**
+ * @title WhitelistAdminRole
+ * @dev WhitelistAdmins are responsible for assigning and removing Whitelisted accounts.
+ */
+contract WhitelistAdminRole is Context {
+    using Roles for Roles.Role;
+
+    event WhitelistAdminAdded(address[] account);
+    event WhitelistAdminRemoved(address indexed account);
+
+    Roles.Role private _whitelistAdmins;
+
+    constructor () internal {
+        address[] memory admins = new address[](1);
+        admins[0] = _msgSender();
+        _addWhitelistAdmin(admins);
+    }
+
+    modifier onlyWhitelistAdmin() {
+        require(isWhitelistAdmin(_msgSender()), "WhitelistAdminRole: caller does not have the WhitelistAdmin role");
+        _;
+    }
+
+    function isWhitelistAdmin(address account) public view returns (bool) {
+        return _whitelistAdmins.has(account);
+    }
+
+    function addWhitelistAdmin(address[] memory account) public onlyWhitelistAdmin {
+        _addWhitelistAdmin(account);
+    }
+
+    function renounceWhitelistAdmin() public {
+        _removeWhitelistAdmin(_msgSender());
+    }
+
+    function _addWhitelistAdmin(address[] memory account) internal {
+        _whitelistAdmins.add(account);
+        emit WhitelistAdminAdded(account);
+    }
+
+    function _removeWhitelistAdmin(address account) internal {
+        _whitelistAdmins.remove(account);
+        emit WhitelistAdminRemoved(account);
+    }
+}
---
+++ contracts/v6/interface/EACAggregatorProxy.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: Unlicensed
+pragma solidity ^0.6.12;
+
+interface EACAggregatorProxy {
+    function latestAnswer() external view returns (int256); 
+    function decimals() external view returns (uint8);
+}
