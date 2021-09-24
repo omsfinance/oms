295908496966b83da17728fe2c3e4584ec89912f
Crypto Shifu <cryptoshifu@gmail.com>
2021-09-25 07:09:11 +0800

    Move calculateReferenceRate into the OraclePrice contract (Again)

--- contracts/v4/interface/IOraclePrice.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity 0.4.24;
-pragma experimental ABIEncoderV2;
-
-import "../library/OraclePriceStruct.sol";
-
-interface IOraclePrice {
-    function getOracleInfoCount() external view returns (uint256);
-    function oracleInfo(uint256 _pid) external view returns (OraclePriceStruct.OracleInfo memory);
-}
--- contracts/v6/interface/IOmsPolicy.sol
+++ contracts/v6/interface/IOmsPolicy.sol
@@ -4,5 +4,4 @@ pragma solidity =0.6.12;
 interface IOmsPolicy {
     function setTargetPrice(uint256 _targetPrice) external;
     function targetPrice() external view returns (uint256);
-    function calculateReferenceRate() external returns(uint256); 
 }
